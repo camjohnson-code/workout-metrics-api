905873903e5a59b40fa5a47fc3203c3da9cd4e1d
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-16 11:04:00 -0700

    Add sample data

---
+++ Data/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 16967528,
+    username: null,
+    resource_state: 2,
+    firstname: 'Cameron',
+    lastname: 'Johnson',
+    bio: '',
+    city: 'Denver',
+    state: 'Colorado',
+    country: 'United States',
+    sex: 'M',
+    premium: false,
+    summit: false,
+    created_at: '2016-08-16T04:23:56Z',
+    updated_at: '2024-01-30T19:59:50Z',
+    badge_type_id: 0,
+    weight: 72.5748,
+    profile_medium:
+      'https://dgalywyr863hv.cloudfront.net/pictures/athletes/16967528/13010397/2/medium.jpg',
+    profile:
+      'https://dgalywyr863hv.cloudfront.net/pictures/athletes/16967528/13010397/2/large.jpg',
+    friend: null,
+    follower: null,
+  },
+];
