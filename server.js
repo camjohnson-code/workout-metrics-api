4c87d2126b0d96d05469f5dd396e0eeb0aa2cfc9
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-23 20:53:56 -0700

    Refactor app.locals and api calls

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const users = require('./Data/users');
-const activities = require('./Data/activities');
 const { v4: uuidv4 } = require('uuid');
 
 app.use(
@@ -13,7 +11,10 @@ app.use(
 app.use(express.json());
 app.set('port', process.env.PORT || 3001);
 
-app.locals.data = [users, activities];
+app.locals = {
+  users: [],
+  activities: [],
+};
 
 app.listen(app.get('port'), () => {
   console.log(
@@ -39,13 +40,13 @@ const checkRequiredProperties = (props) => (req, res, next) => {
 // Endpoints
 // Get all users
 app.get('/api/v1/users', (req, res) => {
-  return res.status(200).json({ data: app.locals.data[0] });
+  return res.status(200).json({ users: app.locals.users });
 });
 
 // Get user by ID
 app.get('/api/v1/users/:id', (req, res) => {
   const userId = parseInt(req.params.id);
-  const users = app.locals.data[0];
+  const users = app.locals.users;
   const user = users.find((user) => user.id === userId);
 
   if (!user) return res.status(404).json({ message: 'User not found' });
@@ -55,23 +56,23 @@ app.get('/api/v1/users/:id', (req, res) => {
 
 // Get all activities
 app.get('/api/v1/activities', (req, res) => {
-  return res.status(200).json({ data: app.locals.data[1] });
+  return res.status(200).json({ activities: app.locals.activities });
 });
 
 // Get all activities or activities for a specific user by ID
 app.get('/api/v1/activities/:userId', (req, res) => {
-    const userId = parseInt(req.params.userId);
-    const activities = app.locals.data[1];
-    const userActivities = activities.filter(
-      (activity) => activity.athlete.id === userId
-    );
+  const userId = parseInt(req.params.userId);
+  const activities = app.locals.activities;
+  const userActivities = activities.filter(
+    (activity) => activity.athlete.id === userId
+  );
 
-    return res.status(200).json({ data: userActivities });
-  }
-);
+  return res.status(200).json({ data: userActivities });
+});
 
 // Add a user
-app.post('/api/v1/users',
+app.post(
+  '/api/v1/users',
   checkRequiredProperties([
     'firstname',
     'lastname',
@@ -82,25 +83,17 @@ app.post('/api/v1/users',
   ]),
   (req, res) => {
     const newUser = { ...req.body };
-    app.locals.data[0].push(newUser);
+    if (!app.locals.users.find((user) => user.id === newUser.id))
+      app.locals.users.push(newUser);
+
     res.status(201).json(newUser);
   }
 );
 
 // Add activities
-app.post('/api/v1/activities',
-  checkRequiredProperties([
-    'userId',
-    'name',
-    'distance',
-    'type',
-    'start_date',
-    'time',
-  ]),
-  (req, res) => {
-    const id = uuidv4();
-    const newActivity = { id, ...req.body };
-    app.locals.data[1].push(newActivity);
-    res.status(201).json(newActivity);
-  }
-);
+app.post('/api/v1/activities', (req, res) => {
+  const id = uuidv4();
+  const newActivity = { id, ...req.body };
+  app.locals.activities.push(newActivity);
+  res.status(201).json(newActivity);
+});
