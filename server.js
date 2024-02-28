a8d595d727ff6c65ac6ce764ae1435147359b5c4
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-28 16:01:46 -0700

    Add and refactor endpoints

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.set('port', process.env.PORT || 3001);
 app.locals = {
   users: [],
   activities: [],
+  quote: {},
+  hallOfFame: [],
 };
 
 app.listen(app.get('port'), () => {
@@ -38,6 +40,11 @@ const checkRequiredProperties = (props) => (req, res, next) => {
 };
 
 // Endpoints
+// Get a quote
+app.get('/api/v1/quote', (req, res) => {
+  return res.status(200).json(app.locals.quote);
+});
+
 // Get all users
 app.get('/api/v1/users', (req, res) => {
   return res.status(200).json({ users: app.locals.users });
@@ -59,41 +66,96 @@ app.get('/api/v1/activities', (req, res) => {
   return res.status(200).json({ activities: app.locals.activities });
 });
 
+// Get all hall of fame activities
+app.get('/api/v1/hallOfFame', (req, res) => {
+  return res.status(200).json({ hallOfFame: app.locals.hallOfFame });
+});
+
+// Get hall of fame activities by user ID
+app.get('/api/v1/hallOfFame/:userId', (req, res) => {
+  const userId = parseInt(req.params.userId);
+  const hallOfFame = app.locals.hallOfFame;
+  const userHallOfFame = hallOfFame.filter(
+    (activity) => activity.userId === userId
+  );
+
+  return res.status(200).json({ activities: userHallOfFame });
+});
+
 // Get all activities or activities for a specific user by ID
 app.get('/api/v1/activities/:userId', (req, res) => {
   const userId = parseInt(req.params.userId);
   const activities = app.locals.activities;
   const userActivities = activities.filter(
-    (activity) => activity.athlete.id === userId
+    (activity) => activity.userId === userId
   );
 
   return res.status(200).json({ data: userActivities });
 });
 
 // Add a user
-app.post(
-  '/api/v1/users',
-  checkRequiredProperties([
-    'firstname',
-    'lastname',
-    'city',
-    'state',
-    'profile_medium',
-    'id',
-  ]),
-  (req, res) => {
-    const newUser = { ...req.body };
-    if (!app.locals.users.find((user) => user.id === newUser.id))
-      app.locals.users.push(newUser);
+app.post('/api/v1/users', (req, res) => {
+  const newUser = { ...req.body };
 
-    res.status(201).json(newUser);
+  const existingUser = app.locals.users.find(
+    (user) => user.id === newUser.id
+  );
+
+  if (!existingUser) {
+    app.locals.users.push(newUser);
+  } else {
+    if (existingUser.stravaAccessToken !== newUser.stravaAccessToken) existingUser.stravaAccessToken = newUser.stravaAccessToken;
+    else if (existingUser.stravaRefreshToken !== newUser.stravaRefreshToken) existingUser.stravaRefreshToken = newUser.stravaRefreshToken;
+    else return res.status(409).json({ message: 'User already exists' });
   }
-);
+
+  const { stravaAccessToken, stravaRefreshToken, ...responseUser } = newUser;
+
+  res.status(201).json(responseUser);
+});
 
 // Add activities
 app.post('/api/v1/activities', (req, res) => {
+  const newActivity = { ...req.body };
+
+  const existingActivity = app.locals.activities.find(
+    (activity) => activity.id === newActivity.id
+  );
+
+  if (!existingActivity) {
+    app.locals.activities.push(newActivity);
+    res.status(201).json(newActivity);
+  }
+});
+
+// Add activities to hall of fame
+app.post('/api/v1/hallOfFame', (req, res) => {
+  const newFavorite = { ...req.body };
+  const existingActivity = app.locals.hallOfFame.find(
+    (activity) => activity.id === newFavorite.id
+  );
+  if (!existingActivity) {
+    app.locals.hallOfFame.push(newFavorite);
+    res.status(201).json(newFavorite);
+  }
+});
+
+// Add a quote
+app.post('/api/v1/quote', (req, res) => {
   const id = uuidv4();
-  const newActivity = { id, ...req.body };
-  app.locals.activities.push(newActivity);
-  res.status(201).json(newActivity);
+  const date = new Date();
+  const newQuote = { id, date, ...req.body };
+  app.locals.quote = newQuote;
+  return res.status(200).json(app.locals.quote);
+});
+
+// Delete from hall of fame
+app.delete('/api/v1/hallOfFame/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const index = app.locals.hallOfFame.findIndex(activity => activity.id === id);
+  if (index === -1) {
+    return res.status(404).json({ error: 'Activity not found' });
+  }
+  app.locals.hallOfFame.splice(index, 1);
+  return res.status(204).send();
 });
