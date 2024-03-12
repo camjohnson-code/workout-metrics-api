a355230d8541179287c4f3f8441fb79cd18c338a
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-03-12 09:36:41 -0600

    Refactor endpoints for database

--- server.js
+++ server.js
@@ -1,185 +1,203 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
+const mongoose = require('mongoose');
+const Activity = require('./models/activity.model');
+const User = require('./models/user.model');
+const Quote = require('./models/quote.model');
+const HallOfFame = require('./models/hallOfFame.model');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI;
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log('connected to database');
+    app.listen(app.get('port'), () => {
+      console.log(
+        `Workout Metrics API running on localhost:${app.get('port')}`
+      );
+    });
+  })
+  .catch(() => {
+    console.log('connection failed');
+  });
 
 app.use(
   cors({
-    origin: 'https://workout-metrics.vercel.app'
+    origin: process.env.CORS_ORIGIN,
   })
 );
 app.use(express.json());
 app.set('port', process.env.PORT || 3001);
 
-app.locals = {
-  users: [],
-  activities: [],
-  quote: {},
-  hallOfFame: [],
-};
-
-app.listen(app.get('port'), () => {
-  console.log(
-    `Workout Metrics API running on https://workout-metrics.vercel.app:${app.get('port')}`
-  );
-});
-
-const checkRequiredProperties = (props) => (req, res, next) => {
-  const reqPropsList = Object.keys(req.body);
-  const hasRequiredProps = props.every((prop) => reqPropsList.includes(prop));
-
-  if (!hasRequiredProps) {
-    const missingProps = props.filter((prop) => !reqPropsList.includes(prop));
-
-    return res
-      .status(422)
-      .send(`The following properties are missing: ${missingProps.join(', ')}`);
-  }
-
-  next();
-};
-
 // Endpoints
-// Get a quote
-app.get('/api/v1/quote', (req, res) => {
-  return res.status(200).json(app.locals.quote);
-});
-
 // Get all users
-app.get('/api/v1/users', (req, res) => {
-  return res.status(200).json({ users: app.locals.users });
+app.get('/api/v1/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Get user by ID
-app.get('/api/v1/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const users = app.locals.users;
-  const user = users.find((user) => user.id === userId);
-
-  if (!user) return res.status(404).json({ message: 'User not found' });
-
-  return res.status(200).json({ data: user });
+app.get('/api/v1/users/:id', async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Get all activities
-app.get('/api/v1/activities', (req, res) => {
-  return res.status(200).json({ activities: app.locals.activities });
+app.get('/api/v1/activities', async (req, res) => {
+  try {
+    const activities = await Activity.find();
+    res.status(200).json(activities);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Get activities by user ID
+app.get('/api/v1/activities/:userId', async (req, res) => {
+  const userId = parseInt(req.params.userId);
+
+  try {
+    const activities = await Activity.find({ userId: userId });
+    res.status(200).json(activities);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // Get all hall of fame activities
-app.get('/api/v1/hallOfFame', (req, res) => {
-  return res.status(200).json({ hallOfFame: app.locals.hallOfFame });
+app.get('/api/v1/hallOfFame', async (req, res) => {
+  try {
+    const hallOfFame = await HallOfFame.find();
+    res.status(200).json(hallOfFame);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Get hall of fame activities by user ID
-app.get('/api/v1/hallOfFame/:userId', (req, res) => {
+app.get('/api/v1/hallOfFame/:userId', async (req, res) => {
   const userId = parseInt(req.params.userId);
-  const hallOfFame = app.locals.hallOfFame;
-  const userHallOfFame = hallOfFame.filter(
-    (activity) => activity.userId === userId
-  );
 
-  return res.status(200).json({ activities: userHallOfFame });
-});
-
-// Get all activities or activities for a specific user by ID
-app.get('/api/v1/activities/:userId', (req, res) => {
-  const userId = parseInt(req.params.userId);
-  const activities = app.locals.activities;
-  const userActivities = activities.filter(
-    (activity) => activity.userId === userId
-  );
-
-  return res.status(200).json({ data: userActivities });
-});
-
-// Add a user
-app.post('/api/v1/users', (req, res) => {
-  const newUser = { ...req.body };
-
-  const existingUser = app.locals.users.find(
-    (user) => user.id === newUser.id
-  );
-
-  if (!existingUser) {
-    app.locals.users.push(newUser);
-  } else {
-    if (existingUser.stravaAccessToken !== newUser.stravaAccessToken) existingUser.stravaAccessToken = newUser.stravaAccessToken;
-    else if (existingUser.stravaRefreshToken !== newUser.stravaRefreshToken) existingUser.stravaRefreshToken = newUser.stravaRefreshToken;
-    else return res.status(409).json({ message: 'User already exists' });
-  }
-
-  const { stravaAccessToken, stravaRefreshToken, ...responseUser } = newUser;
- 
-  res.status(201).json(responseUser);
-});
-
-// Add activities
-app.post('/api/v1/activities', (req, res) => {
   try {
-    const newActivity = { ...req.body };
+    const hallOfFame = await HallOfFame.find({ userId: userId });
+    res.status(200).json(hallOfFame);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
 
-    const existingActivity = app.locals.activities.find(
-      (activity) => activity.id === newActivity.id
-    );
-
-    if (!existingActivity) {
-      app.locals.activities.push(newActivity);
-      res.status(201).json(newActivity);
-    } else {
-      res.status(200).json({ message: 'Activity already exists' });
-    }
+// Get a quote
+app.get('/api/v1/quote', async (req, res) => {
+  try {
+    const quote = await Quote.find();
+    res.status(200).json(quote);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: error.message });
   }
 });
 
-// Add activities to hall of fame
-app.post('/api/v1/hallOfFame', (req, res) => {
-  const newFavorite = { ...req.body };
-  const existingActivity = app.locals.hallOfFame.find(
-    (activity) => activity.id === newFavorite.id
-  );
-  if (!existingActivity) {
-    app.locals.hallOfFame.push(newFavorite);
+// Create a user
+app.post('/api/v1/users', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Create an activity
+app.post('/api/v1/activities', async (req, res) => {
+  try {
+    const existingActivity = await Activity.findOne({ id: req.body.id });
+    if (existingActivity)
+      return res.status(400).json({ error: 'Activity already exists' });
+
+    const newActivity = await Activity.create(req.body);
+    res.status(201).json(newActivity);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Create a hall of fame activity
+app.post('/api/v1/hallOfFame', async (req, res) => {
+  try {
+    const newFavorite = await HallOfFame.create(req.body);
     res.status(201).json(newFavorite);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
-// Add a quote
-app.post('/api/v1/quote', (req, res) => {
-  const id = uuidv4();
-  const date = new Date();
-  const newQuote = { id, date, ...req.body };
-  app.locals.quote = newQuote;
-  return res.status(200).json(app.locals.quote);
+// Create a quote
+app.post('/api/v1/quote', async (req, res) => {
+  try {
+    const newQuote = await Quote.create(req.body);
+    res.status(201).json(newQuote);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Update a user
-app.put('/api/v1/users/:id', (req, res) => {
-  const userId = parseInt(req.params.id);
-  const users = app.locals.users;
-  const userIndex = users.findIndex((user) => user.id === userId);
-
-  if (userIndex === -1) return res.status(404).json({ message: 'User not found' });
-
-  const updatedUser = { ...users[userIndex], ...req.body };
-  users[userIndex] = updatedUser;
-
-  const { stravaAccessToken, stravaRefreshToken, expirationToken, ...responseUser } = updatedUser;
-
-  return res.status(200).json(responseUser);
+app.put('/api/v1/users/:id', async (req, res) => {
+  try {
+    const user = await User.findOneAndUpdate({ id: req.params.id }, req.body, {
+      new: true,
+    });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Delete from hall of fame
-app.delete('/api/v1/hallOfFame/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const index = app.locals.hallOfFame.findIndex(activity => activity.id === id);
-  if (index === -1) {
-    return res.status(404).json({ error: 'Activity not found' });
+app.delete('/api/v1/hallOfFame/:id', async (req, res) => {
+  try {
+    const hallOfFame = await HallOfFame.findOneAndDelete({ id: req.params.id });
+    if (!hallOfFame)
+      return res.status(404).json({ error: 'Activity not found' });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  app.locals.hallOfFame.splice(index, 1);
-  return res.status(204).send();
 });
- 
+
+// Delete a user
+app.delete('/api/v1/users/:id', async (req, res) => {
+  try {
+    const user = await User.findOneAndDelete({ id: req.params.id });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete activities by user Id
+app.delete('/api/v1/activities/:userId', async (req, res) => {
+  try {
+    const activities = await Activity.deleteMany({ userId: req.params.userId });
+    if (!activities)
+      return res.status(404).json({ error: 'Activities not found' });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
